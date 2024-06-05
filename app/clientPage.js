6d6888e872a7c97c0eb01b9d0fc7f2f66733983c
Stanislav <s.v.pukha@gmail.com>
2024-06-05 16:40:04 +0300

    components init

--- app/clientPage.js
+++ app/clientPage.js
@@ -1,3 +1,9 @@
+import { Input } from "@/components/ui/input";
+
 export default function ClientPage() {
-  return <div>Client Page</div>;
+  return (
+    <div className="text-3xl flex flex-col items-center">
+      <Input type="file" className="w-1/2 bg-background text-3xl" />
+    </div>
+  );
 }
