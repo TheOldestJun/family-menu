3a40c00bd135fd8c356ca0f7b29557c312d803bf
Stanislav <s.v.pukha@gmail.com>
2024-06-20 12:46:37 +0300

    deduplication problem solved

--- app/api/shoplist/add/route.js
+++ app/api/shoplist/add/route.js
@@ -5,11 +5,26 @@ import { connect } from "react-redux";
 export async function POST(request) {
   const body = await request.json();
   try {
-    //TODO: deduplicate shoplist items
-    const result = await prisma.shopList.createMany({
-      data: [...body.map((item) => ({ productId: item.id }))],
-      skipDuplicates: true,
+    // Fetch existing product IDs from the ShopList table
+    const existingProductIds = await prisma.shopList.findMany({
+      select: { productId: true },
     });
+
+    // Create a Set of existing product IDs
+    const existingProductIdSet = new Set(
+      existingProductIds.map((item) => item.productId)
+    );
+
+    // Filter out the new product IDs that already exist in the database
+    const uniqueProductIds = body.filter(
+      (item) => !existingProductIdSet.has(item.id)
+    );
+
+    // Create new ShopList entries with unique product IDs
+    const result = await prisma.shopList.createMany({
+      data: uniqueProductIds.map((item) => ({ productId: item.id })),
+    });
+
     return NextResponse.json(result);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
